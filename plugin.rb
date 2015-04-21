2b18feca9a363261c3b12216c7eed8b0179843ed
Vladimír Oraný <vladimir@orany.cz>
2015-04-21 11:47:09 +0200

    created plugin

---
+++ plugin.rb
@@ -0,0 +1,8 @@
+# name: Discourse ALLOWALL
+# about: Sets X-Frame-Options to ALLOWALL 
+# version: 1
+# authors: riking, dconjar, musketyr
+
+...
+Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
+...
